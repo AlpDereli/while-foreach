b58d16ce7e3f5e1526b2246646d7238d828992ac
AlpDereli <alpdereli26@gmail.com>
2023-07-30 01:19:18 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Sayi: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+int sayac =1, toplam = 0;
+while (sayac<=sayi)
+{
+    toplam+= sayac;
+    sayac++;
+}
+Console.WriteLine(toplam/sayi);
+
+char c ='a';
+while (c < 'z')
+{
+    Console.WriteLine(c);
+    c++;
+}
+
+string[] araba = {"BMV", "Ford", "Toyota", "Nissan"};
+foreach(var ar in araba)
+{
+    Console.WriteLine(ar);
+}
+
